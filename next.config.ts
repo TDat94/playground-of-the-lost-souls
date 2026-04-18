00c88643802eff957da0cab2e439f7618721ae6c
Muramasa94 <22127057@student.hcmus.edu.vn>
2026-04-18 10:29:50 +0700

    Additional setups

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // for github pages
+  images: {
+    unoptimized: true, // since github pages doesn't support image optimization
+  },
 };
 
 export default nextConfig;
