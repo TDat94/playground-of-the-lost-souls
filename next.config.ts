f1fcd0c9ebb518ae47397c7abd228637cb133c8b
Muramasa94 <22127057@student.hcmus.edu.vn>
2026-04-18 10:53:47 +0700

    Added basePath for github page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // for github pages
+  basePath: '/playground-of-the-lost-souls',
   images: {
     unoptimized: true, // since github pages doesn't support image optimization
   },
